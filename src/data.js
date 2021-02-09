8197bcdba6a1b84f2c973f31bc04468b4d6ecc81
Zina Mohamed <zina.a.mohamed@gmail.com>
2021-02-08 19:13:07 -0800

    work in progress

---
+++ src/data.js
@@ -0,0 +1,24 @@
+ const ingredientsArray = [
+  {
+    ingredient: 'Pizza Dough',
+    quantity: '1 ball',
+  },
+  {
+    ingredient: 'Margherita Sauce',
+    quantity: '500ml',
+  },
+  {
+    ingredient: 'Mozarella Cheese',
+    quantity: '20oz',
+  },
+  {
+    ingredient: 'Tomatoes',
+    quantity: '3 ripe',
+  },
+  {
+    ingredient: 'Basil',
+    quantity: '1 handful',
+  },
+]
+
+export default ingredientsArray;
